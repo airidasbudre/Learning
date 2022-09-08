3ba7c24a0ee9ed965876a7dd7be99f53f1f04f0c
airidasbudre <107766475+airidasbudre@users.noreply.github.com>
2022-09-08 23:18:31 +0300

    Add files via upload

---
+++ Reserved words.py
@@ -0,0 +1,20 @@
+Lambda 
+
+example:
+
+squares = lambda x: x*x
+print('Using lambda: ', squares(5))
+
+
+Assert
+
+#The assert keyword is used when debugging code.
+#The assert keyword lets you test if a condition in your code returns True, if not, the program will raise an AssertionError.
+#You can write a message to be written if the code returns False, check the example below.
+
+
+Nonlocal
+
+#The nonlocal keyword is used to work with variables inside nested functions, where the variable should not belong to the inner function.
+#Use the keyword nonlocal to declare that the variable is not local.
+
