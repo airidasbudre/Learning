7a8b6f12c4761ffa0d6d9bd448c0fab981e321b4
airidasbudre <107766475+airidasbudre@users.noreply.github.com>
2022-11-26 23:46:43 +0200

    Update VScode.keybordShortcut.py

--- VScode.keybordShortcut.py
+++ VScode.keybordShortcut.py
@@ -60,3 +60,25 @@
 # Ctrl+K Ctrl+D Move last selection to next Find match
 # Alt+C / R / W Toggle case-sensitive / regex / whole word
 #----------------------------------------
+
+# Multi-cursor and selection
+# ---------------------------------------
+# Alt+Click Insert cursor
+# Ctrl+Alt+ ↑ / ↓ Insert cursor above / below
+# Ctrl+U Undo last cursor operation
+# Shift+Alt+I Insert cursor at end of each line selected
+# Ctrl+L Select current line
+# Ctrl+Shift+L Select all occurrences of current selection
+# Ctrl+F2 Select all occurrences of current word
+# Shift+Alt+→ Expand selection
+# Shift+Alt+← Shrink selection
+# Shift+Alt +
+# (drag mouse)
+# Column (box) selection
+# Ctrl+Shift+Alt
+# + (arrow key)
+# Column (box) selection
+# Ctrl+Shift+Alt
+# +PgUp/PgDn
+# Column (box) selection page up/down
+# ---------------------------------------
